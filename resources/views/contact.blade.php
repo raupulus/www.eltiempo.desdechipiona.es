e6017dde6e52b6c2c887132a1139beedbbf6d8c8
Raúl Caro Pastorino <dev@fryntiz.es>
2019-08-06 19:25:12 +0200

    Integrando contacto

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -19,8 +19,35 @@
 @section('content')
     @include('layouts.breadcrumbs')
     <div class="row">
-        <div class="col-md-8 mb-5">
-            Página de pruebas en blanco
+
+        <div class="col-md-8 mb-5 contact-form">
+            <div class="contact-image">
+                <img src="https://image.ibb.co/kUagtU/rocket_contact.png" alt="rocket_contact"/>
+            </div>
+            <form method="post">
+                <h3>Drop Us a Message</h3>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <input type="text" name="txtName" class="form-control" placeholder="Your Name *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="txtEmail" class="form-control" placeholder="Your Email *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="txtPhone" class="form-control" placeholder="Your Phone Number *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="btnSubmit" class="btnContact" value="Send Message" />
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <textarea name="txtMsg" class="form-control" placeholder="Your Message *" style="width: 100%; height: 150px;"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
 
         <div class="col-md-4 mb-5">
